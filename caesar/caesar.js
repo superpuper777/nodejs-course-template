0dae72d857d597e949c809277ba58f3706c82b06
superpuper777 <superfevrik@gmail.com>
2020-11-30 16:48:56 +0300

    Delete comments

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,21 +1,3 @@
-// const alphabetLength = 26;
-// exports.caesarShift = (str, shift) => {
-//   let output = '';
-//   for (let i = 0; i < str.length; i++) {
-//     let c = str[i];
-//     if (c.match(/[a-z]/i)) {
-//       const code = str.charCodeAt(i);
-//       if (code >= 65 && code <= 90) {
-//         c = String.fromCharCode(((code - 65 + shift) % alphabetLength) + 65);
-//       } else if (code >= 97 && code <= 122) {
-//         c = String.fromCharCode(((code - 97 + shift) % alphabetLength) + 97);
-//       }
-//     }
-//     output += c;
-//   }
-//   return output;
-// };
-
 const abc = 'abcdefghijklmnopqrstuvwxyz';
 const abcUpper = abc.toUpperCase();
 
